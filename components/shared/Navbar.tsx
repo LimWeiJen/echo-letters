dadf2811a1cc2ed8815ab0717f5558b18ffb04b9
LimWeiJen <limweijen96@gmail.com>
2024-09-08 14:14:05 +0800

    feat: Add user button in navbar

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,12 +1,17 @@
+"use client"
+
 import { createEmptyLetter } from '@/lib/actions/letters.actions'
 import { Home, Inbox, MailIcon, MailOpenIcon, MailPlusIcon, Pencil, Plus, Settings } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
+import { SignedIn, UserButton } from "@clerk/nextjs"
 
 const Navbar = ({ user }: { user: any }) => {
   return (
     <nav className='flex w-screen justify-center gap-x-4 p-5'>
-      <h1>{user.username}</h1>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
       <Link href='/home'>
         <Home />
       </Link>
